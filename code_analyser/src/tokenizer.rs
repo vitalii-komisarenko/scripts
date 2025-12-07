032dfec34f3926bc1ba41e50bfcba167f66e6345
Your Name <vitalii.komisarenko@gmail.com>
2025-12-07 11:48:28 +0100

    code_analyser: tokenizer: Tokenize whitespace

--- code_analyser/src/tokenizer.rs
+++ code_analyser/src/tokenizer.rs
@@ -5,6 +5,7 @@ use std::string::String;
 #[derive(PartialEq)]
 enum Token
 {
+    None,
     PreprocessorDirective(String),
     LineContinuation(String),
     NewLine(String),
@@ -17,9 +18,43 @@ enum Token
 }
 
 
-pub fn tokenize(s: &str) -> Vec<Token>
+fn read_whitespace(mut s: &str) -> String
 {
+    let mut res = String::new();
+
+    while s.len() > 0
+    {
+        let ch = s.bytes().nth(0).unwrap();
+
+        if ch != b' ' && ch != b'\t'
+        {
+            break;
+        }
+
+        res.push(ch as char);
+        s = &s[1..];
+    }
+
+    res
+}
+
+
+pub fn tokenize(file_content: &str) -> Vec<Token>
+{
+    let mut s = file_content;
     let mut res = Vec::<Token>::new();
+
+    while s.len() > 0
+    {
+        let ch = s.bytes().nth(0).unwrap();
+
+        if ch == b' ' || ch == b'\t'
+        {
+            let val = read_whitespace(s);
+            s = &s[val.len()..];
+            res.push(Token::WhiteSpace(val));
+        }
+    }
 
     res
 }
@@ -28,11 +63,29 @@ pub fn tokenize(s: &str) -> Vec<Token>
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::tokenizer;
+    use crate::tokenizer::Token;
 
     #[test]
     fn test_empty() {
         let input = "";
-        assert_eq!(tokenize(input), Vec::<tokenizer::Token>::new());
+        assert_eq!(tokenize(input), Vec::<Token>::new());
+    }
+
+    #[test]
+    fn test_whitespace_space() {
+        let input = " ";
+        assert_eq!(tokenize(input), vec![Token::WhiteSpace(" ".to_string())]);
+    }
+
+    #[test]
+    fn test_whitespace_tab() {
+        let input = "\t";
+        assert_eq!(tokenize(input), vec![Token::WhiteSpace("\t".to_string())]);
+    }
+
+    #[test]
+    fn test_whitespace_mixed() {
+        let input = " \t  \t  \t\t\t";
+        assert_eq!(tokenize(input), vec![Token::WhiteSpace(" \t  \t  \t\t\t".to_string())]);
     }
 }
