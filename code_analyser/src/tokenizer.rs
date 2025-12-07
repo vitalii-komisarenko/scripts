2d0b05e5b3e3e9f34af0b4ce0a1cacb71d62596d
Your Name <vitalii.komisarenko@gmail.com>
2025-12-07 11:48:28 +0100

    code_analyser: tokenizer: Tokenize numbers

--- code_analyser/src/tokenizer.rs
+++ code_analyser/src/tokenizer.rs
@@ -5,14 +5,26 @@ use std::string::String;
 #[derive(PartialEq)]
 enum Token
 {
+    // A token that is not properly parsed
     Unknown(String),
     PreprocessorDirective(String),
+    // Backshash followed by newline markers, such as \n, \r, \n\r, \r\n
     LineContinuation(String),
+    // \n, \r, \n\r or \r\n
     NewLine(String),
+    // Whitespace, but not Token::NewLine
     WhiteSpace(String),
+    // Single-line or multiline comment
+    // Markers of the begin and end (//, newline, /* and */) are included
     Comment(String),
+    // String
+    // Quotes are included
     String(String),
+    // Char
+    // Quotes are included
     Char(String),
+    // Integer or floating number, but without leading sign
+    // E.g. 123456 is a number, 123e-456 is a number, but -123 is a combination of an operator "-" and number 123.
     Number(String),
     Operator(String),
 }
@@ -154,6 +166,76 @@ fn read_char(mut s: &str) -> String
 }
 
 
+fn read_unsigned_int(mut s: &str) -> String
+{
+    let mut res = String::new();
+
+    while s.len() > 0
+    {
+        let ch = s.bytes().nth(0).unwrap();
+        if b'0' <= ch && ch <= b'9'
+        {
+            res.push(ch as char);
+            s = &s[1..];
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    res
+}
+
+
+fn read_number(mut s: &str) -> String
+{
+    let mut res = String::new();
+
+    // Integer part
+    let mut val = read_unsigned_int(s);
+    res.push_str(&s[..val.len()]);
+    s = &s[val.len()..];
+
+    // Read dot
+    if s.starts_with(".")
+    {
+        res.push_str(&s[..1]);
+        s = &s[1..];
+    }
+
+    // Fractional part
+    val = read_unsigned_int(s);
+    res.push_str(&s[..val.len()]);
+    s = &s[val.len()..];
+
+    // Exponent symbol
+    if s.starts_with("e") || s.starts_with("E")
+    {
+        res.push_str(&s[..1]);
+        s = &s[1..];
+    }
+    else
+    {
+        return res;
+    }
+
+    // Exponent sign
+    if s.starts_with("-") || s.starts_with("+")
+    {
+        res.push_str(&s[..1]);
+        s = &s[1..];
+    }
+
+    // Exponent
+    val = read_unsigned_int(s);
+    res.push_str(&s[..val.len()]);
+    s = &s[val.len()..];
+
+    res
+}
+
+
 pub fn tokenize(file_content: &str) -> Vec<Token>
 {
     let mut s = file_content;
@@ -220,6 +302,16 @@ pub fn tokenize(file_content: &str) -> Vec<Token>
             let val = read_char(s);
             s = &s[val.len()..];
             res.push(Token::Char(val.to_string()));
+            continue 'outer;
+        }
+
+        if (b'0' <= ch && ch <= b'9') ||
+           s.starts_with(".0") || s.starts_with(".1") || s.starts_with(".2") || s.starts_with(".3") || s.starts_with(".4") ||
+           s.starts_with(".5") || s.starts_with(".6") || s.starts_with(".7") || s.starts_with(".8") || s.starts_with(".9")
+        {
+            let val = read_number(s);
+            s = &s[val.len()..];
+            res.push(Token::Number(val.to_string()));
             continue 'outer;
         }
 
@@ -369,4 +461,95 @@ mod test {
             Token::Char("'\\''".to_string()),
             Token::WhiteSpace("    ".to_string()),
         ]);
-    }}
+    }
+
+    #[test]
+    fn test_number_simple_1() {
+        let input = "  1    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("1".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_simple_2() {
+        let input = "  12345    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("12345".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_float_1() {
+        let input = "  1.2    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("1.2".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_float_2() {
+        let input = "  1.    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("1.".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_float_3() {
+        let input = "  .2    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number(".2".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_float_4() {
+        let input = "  123.456    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("123.456".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_exponent_1() {
+        let input = "  123.456e789    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("123.456e789".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_exponent_2() {
+        let input = "  123.456e-789    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("123.456e-789".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+
+    #[test]
+    fn test_number_exponent_3() {
+        let input = "  123.456E+789    ";
+        assert_eq!(tokenize(input), vec![
+            Token::WhiteSpace("  ".to_string()),
+            Token::Number("123.456E+789".to_string()),
+            Token::WhiteSpace("    ".to_string()),
+        ]);
+    }
+}
